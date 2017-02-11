2170b9069a09ebac89403d48008bb2d84e848cdc
Essacnewton <bgicewolves@gmail.com>
2017-02-11 15:10:40 -0500

    all UI finished and most of coding

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Isaac Prunty on 2/11/17.
+//  Copyright © 2017 Isaac Prunty. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,63 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Isaac Prunty on 2/11/17.
+//  Copyright © 2017 Isaac Prunty. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLbl.text = pokemon.name
+        
+        let img = UIImage(named: "\(pokemon.pokedexID)")
+        
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexID)"
+            
+        pokemon.dowloadPokemonDetails {
+            
+            // Whatever we write will only be called after the network call is complete!
+            self.updateUI()
+        }
+        
+    }
+    
+    func updateUI() {
+    
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        
+    }
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    
+}
